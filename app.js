85c32a85775f339109da8305f222c17660aa167c
Job Ugbem <ugbemjob57@gmail.com>
2021-03-05 03:35:43 +0100

    Fix seeder file

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 // const cookieParser = require('cookie-parser')
-const { origin } = require('./config')
-const connectToDatabase = require('./config/database')()
+const { origin } = require("./config");
+require("./config/database")();
 
-const api = require('./api')
-const logger = require('./utils/logger')
+const api = require("./api");
+const logger = require("./utils/logger");
 
-const app = express()
+const app = express();
 
-app.use(morgan('combined', { stream: logger.stream }))
-app.use(helmet())
+app.use(morgan("combined", { stream: logger.stream }));
+app.use(helmet());
 app.use(
 	cors({
 		origin: origin, //url of production app
 		credentials: false, // make true on production
 	}),
-)
+);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // BASIC ROUTING
 
 // app.get('/api/v1', function (_, res) {
 // 	res.json({ message: 'Welcome to our API' })
 // })
-app.use('/api/v1', api)
+app.use("/api/v1", api);
 
-module.exports = app
+module.exports = app;
---
+++ seeders/seed-down.js
@@ -0,0 +1,18 @@
+const db = require("../config/database")();
+const logger = require("../utils/logger");
+
+const faker = require("faker");
+const User = require("../models/users.model");
+
+const destoryData = async () => {
+	try {
+		await User.deleteMany();
+		logger.log("info", "Database Purged!");
+		process.exit();
+	} catch (error) {
+		logger.log("info", `${error}`);
+		process.exit(1);
+	}
+};
+
+if (process.argv[2] === "-d") destoryData();
